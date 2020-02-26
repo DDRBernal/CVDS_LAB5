61a988424d341e5cdc3030d83743a87ceb84aa07
david <otaloradavid.903@gmail.com>
2020-02-25 22:10:46 -0500

    falta parte 3/4

--- lab5/Servlet/src/main/java/edu/eci/cvds/servlet/SampleServlet2.java
+++ lab5/Servlet/src/main/java/edu/eci/cvds/servlet/SampleServlet2.java
@@ -1,5 +1,6 @@
 package edu.eci.cvds.servlet;
 
+import edu.eci.cvds.servlet.model.*;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Optional;
@@ -9,20 +10,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @WebServlet(
     urlPatterns = "/Loquequiera"
 )
 public class SampleServlet2 extends HttpServlet{
     static final long serialVersionUID = 35L;
+	List<Todo> todolist = new ArrayList<>();
 
-    @Override
-   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       Writer responseWriter = resp.getWriter();
-       Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
-       String id = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
- 
-       resp.setStatus(HttpServletResponse.SC_OK);
-       responseWriter.write("id: " + id );
-       responseWriter.flush();
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Writer responseWriter = resp.getWriter();
+		Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+        String id = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
+	    
+		try{
+			if (todo!=null){
+				int ID = Integer.parseInt(id);
+				Todo todo = Service.getTodo(ID);
+				resp.setStatus(HttpServletResponse.SC_OK);
+				todolist.add(todo);
+				responseWriter.write(Service.todosToHTMLTable(todolist));
+				responseWriter.flush();
+			}else{
+				resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+				
+			}
+		}catch (NumberFormatException | IOException e){
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		}catch (MalformedURLException e){
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+        //resp.setStatus(HttpServletResponse.SC_OK);
+        //responseWriter.write("id: " + id );
+        
    }
 }
